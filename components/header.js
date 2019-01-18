e79d69e42d4c07104ef7d1a576d1fb5c57726c1e
Graham Barber <grahammbarber@gmail.com>
2019-01-17 22:35:04 -0800

    feat(header): add new navigation bar

--- components/header.js
+++ components/header.js
@@ -1,5 +1,51 @@
+import Link from 'next/link'
+import { Box, Flex, Text, Button } from '@rebass/emotion'
+
 export default () => (
-  <header>
-    <h1>Next.js Example on Now 2.0</h1>
-  </header>
+  <Flex as='header' py={4} alignItems='center'>
+    <Flex width={1 / 3}>
+      <Box mx={4}>
+        <Link href='/'>
+          <Text
+            as='a'
+            fontFamily='sans'
+            css={{ ':hover': { textDecoration: 'underline' }, fontWeight: 700 }}
+          >
+            OSU Hackathon Club
+          </Text>
+        </Link>
+      </Box>
+    </Flex>
+    <Flex width={2 / 3} justifyContent='flex-end' flexDirection='row' alignItems='center'>
+      <Box mx={4}>
+        <Link href='/about'>
+          <Text
+            as='a'
+            fontFamily='sans'
+            css={{ ':hover': { textDecoration: 'underline' }, fontWeight: 700 }}
+          >
+            About
+          </Text>
+        </Link>
+      </Box>
+      <Box mx={4}>
+        <Link href='/contact'>
+          <Text
+            as='a'
+            fontFamily='sans'
+            css={{ ':hover': { textDecoration: 'underline' }, fontWeight: 700 }}
+          >
+            Contact
+          </Text>
+        </Link>
+      </Box>
+      <Box mx={4}>
+        <Link href='/participate'>
+          <Button as='a' bg='orange'>
+            <Text fontFamily='sans'>Participate</Text>
+          </Button>
+        </Link>
+      </Box>
+    </Flex>
+  </Flex>
 )
