90c6be3fc199b897bf9d9915af8cef24c0cec188
Graham Barber <grahammbarber@gmail.com>
2019-01-17 22:34:34 -0800

    feat(theme): remove apple-system font, add colors

--- theme.js
+++ theme.js
@@ -1,9 +1,12 @@
 export default {
   fonts: {
     sans:
-      "-apple-system, Roboto, BlinkMacSystemFont, 'Segoe UI', Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif"
+      "Roboto, BlinkMacSystemFont, 'Segoe UI', Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif"
   },
   colors: {
-    main: '#00859B'
+    main: '#00859B',
+    orange: '#D73F09',
+    black: '#000000',
+    white: '#FFFFFF'
   }
 }
