2777171de209f7aa6b2b1da5ff05957352b8f7da
Graham Barber <grahammbarber@gmail.com>
2019-03-24 13:41:41 -0700

    feat(index): add responsive styles

--- pages/index.js
+++ pages/index.js
@@ -4,12 +4,22 @@ import { Hero, HeroText } from '../components/hero'
 
 export default () => (
   <Box as='main' width={[1]}>
-    <Hero alignItems='center' justifyContent='flex-start' px={4}>
-      <Flex flexDirection='column'>
-        <HeroText fontSize={8} py={1} px={3} mb={2} css={{ fontWeight: 900 }}>
+    <Hero
+      alignItems='center'
+      justifyContent={['center', 'flex-start']}
+      px={[0, 4]}
+    >
+      <Flex flexDirection='column' alignItems={['center', 'flex-start']}>
+        <HeroText
+          fontSize={[6, 8]}
+          py={1}
+          px={3}
+          mb={2}
+          css={{ fontWeight: 900 }}
+        >
           BeaverHacks
         </HeroText>
-        <HeroText py={2} px={3} fontSize={5} css={{ fontWeight: 100 }}>
+        <HeroText py={2} px={3} fontSize={[4, 5]} css={{ fontWeight: 100 }}>
           Make something cool in a weekend
         </HeroText>
       </Flex>
