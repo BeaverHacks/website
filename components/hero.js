f4ec06d92ac60e42653d74a7b5a43696571924d3
Graham Barber <grahammbarber@gmail.com>
2019-03-24 13:51:49 -0700

    feat(hero): take img as prop, add default color
    Also improves the HeroText drop shadow.

--- components/hero.js
+++ components/hero.js
@@ -1,4 +1,5 @@
 import { Flex, Text } from '@rebass/emotion'
+import { opacify } from 'polished'
 import styled from '@emotion/styled'
 
 export const Hero = styled(Flex)`
@@ -7,12 +8,12 @@ export const Hero = styled(Flex)`
   @media (max-width: 40em) {
     height: 40vh;
   }
-
-  background-image: url('/static/kelley.jpg');
+  background-image: url('${({ image }) => image}');
   background-size: cover;
   background-attachment: fixed;
   background-position: bottom;
   clip-path: polygon(0 0, 1600px 15%, 1600px 100%, 0 85%);
+  background-color: ${({ theme }) => theme.colors.orange}
 `
 
 export const HeroText = styled(Text)`
@@ -20,5 +21,6 @@ export const HeroText = styled(Text)`
   font-family: ${({ theme }) => theme.fonts.sans};
   color: ${({ theme }) => theme.colors.black};
   width: fit-content;
-  box-shadow: 5px 2px 2px 1px rgba(0, 0, 0, 0.2);
+  box-shadow: 5px 5px 12px 1px
+    ${({ theme }) => opacify(-0.6, theme.colors.black)};
 `
