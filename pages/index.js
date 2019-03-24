ce74a6b011920e40580dd0321369c53b09d84d43
Graham Barber <grahammbarber@gmail.com>
2019-03-24 13:19:31 -0700

    refactor(index): use hero component

--- pages/index.js
+++ pages/index.js
@@ -1,45 +1,18 @@
-import { Box, Flex, Heading, Text } from '@rebass/emotion'
+import { Box, Flex } from '@rebass/emotion'
+
+import { Hero, HeroText } from '../components/hero'
 
 export default () => (
   <Box as='main' width={[1]}>
-    <Flex
-      alignItems='center'
-      justifyContent='flex-start'
-      px={4}
-      css={{
-        height: '70vh',
-        backgroundImage: `url('/static/kelley.jpg')`,
-        backgroundSize: 'cover',
-        backgroundPosition: 'bottom',
-        clipPath: 'polygon(0 0, 1600px 15%, 1600px 100%, 0 85%)',
-        verticalAlign: 'center'
-      }}
-    >
+    <Hero alignItems='center' justifyContent='flex-start' px={4}>
       <Flex flexDirection='column'>
-        <Heading
-          bg='white'
-          fontFamily='sans'
-          color='black'
-          fontSize='8'
-          p={1}
-          m={1}
-          width='fit-content'
-          css={{ fontWeight: 900 }}
-        >
+        <HeroText fontSize={8} py={1} px={3} mb={2} css={{ fontWeight: 900 }}>
           BeaverHacks
-        </Heading>
-        <Text
-          bg='white'
-          fontFamily='sans'
-          color='black'
-          p={1}
-          m={1}
-          fontSize='5'
-          width='fit-content'
-        >
+        </HeroText>
+        <HeroText py={2} px={3} fontSize={5} css={{ fontWeight: 100 }}>
           Make something cool in a weekend
-        </Text>
+        </HeroText>
       </Flex>
-    </Flex>
+    </Hero>
   </Box>
 )
