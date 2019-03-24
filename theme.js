7fabcaf62948ed586f8221a13703bf58a33b5d38
Graham Barber <grahammbarber@gmail.com>
2019-03-24 14:16:38 -0700

    feat(theme): darken black, add serif font

--- theme.js
+++ theme.js
@@ -1,12 +1,13 @@
 export default {
   fonts: {
     sans:
-      "Roboto, BlinkMacSystemFont, 'Segoe UI', Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif"
+      "Roboto, BlinkMacSystemFont, 'Segoe UI', Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif",
+    serif: "'Roboto Slab', 'Times New Roman', Times, serif"
   },
   colors: {
     main: '#00859B',
     orange: '#D73F09',
-    black: '#333333',
+    black: '#222222',
     white: '#FFFFFF'
   }
 }
