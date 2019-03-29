86d8e083162520d07fab2d6988e28ce2bb02d2d6
Graham Barber <grahammbarber@gmail.com>
2019-03-28 19:02:07 -0700

    feat(participate): stub page

---
+++ pages/participate.js
@@ -0,0 +1,8 @@
+import { TextContainer, Heading, Text } from '../components/content'
+
+export default () => (
+  <TextContainer>
+    <Heading>Participating</Heading>
+    <Text>Rules/signup and such</Text>
+  </TextContainer>
+)
