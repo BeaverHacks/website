77be53b709ed36b1843002bedf196b708afaf0fc
Graham Barber <grahammbarber@gmail.com>
2019-06-20 20:44:17 -0700

    feat(layouts): add width constraints

--- components/layouts.js
+++ components/layouts.js
@@ -2,19 +2,25 @@ import { Flex } from '@rebass/emotion'
 import { Children } from 'react'
 import { node, bool } from 'prop-types'
 
+const wideWidth = `1140px`
+const narrowWidth = `940px`
+
 const HalfAndHalf = ({ children, reverse, ...props }) => {
   const [first, ...remaining] = Children.toArray(children)
 
   return (
-    <Flex
-      flexDirection={['column', reverse ? 'row-reverse' : 'row']}
-      {...props}
-    >
-      <Flex px={3} width={[1, 1 / 2]} alignItems='center'>
-        {first}
-      </Flex>
-      <Flex px={3} width={[1, 1 / 2]} alignItems='center'>
-        {remaining}
+    <Flex justifyContent='center'>
+      <Flex
+        flexDirection={['column', reverse ? 'row-reverse' : 'row']}
+        width={wideWidth}
+        {...props}
+      >
+        <Flex px={3} width={[1, 1 / 2]}>
+          {first}
+        </Flex>
+        <Flex px={3} width={[1, 1 / 2]}>
+          {remaining}
+        </Flex>
       </Flex>
     </Flex>
   )
@@ -29,15 +35,17 @@ const Thirds = ({ children }) => {
   const [first, second, ...remaining] = Children.toArray(children)
 
   return (
-    <Flex flexDirection={['column', 'row']}>
-      <Flex px={3} width={[1, 1 / 3]} alignItems='center'>
-        {first}
-      </Flex>
-      <Flex px={3} width={[1, 1 / 3]} alignItems='center'>
-        {second}
-      </Flex>
-      <Flex px={3} width={[1, 1 / 3]} alignItems='center'>
-        {remaining}
+    <Flex justifyContent='center'>
+      <Flex flexDirection={['column', 'row']} width={narrowWidth}>
+        <Flex px={3} width={[1, 1 / 3]} alignItems='center'>
+          {first}
+        </Flex>
+        <Flex px={3} width={[1, 1 / 3]} alignItems='center'>
+          {second}
+        </Flex>
+        <Flex px={3} width={[1, 1 / 3]} alignItems='center'>
+          {remaining}
+        </Flex>
       </Flex>
     </Flex>
   )
