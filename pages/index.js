96a97e9dc5cd920f85cf3b9f06fac510a11a803a
Graham Barber <grahammbarber@gmail.com>
2019-06-23 20:04:37 -0700

    feat(home): add dynamic dates retrieval

--- pages/index.js
+++ pages/index.js
@@ -1,74 +1,129 @@
 import { Box, Flex, Button } from '@rebass/emotion'
+import ky from 'ky-universal'
+import PropTypes from 'prop-types'
 
 import { Hero, HeroText } from '../components/hero'
 import { HalfAndHalf } from '../components/layouts'
 import { Heading, Text, Highlight } from '../components/content'
 
-export default () => (
-  <Box as='main' width={[1]}>
-    <Hero
-      alignItems='center'
-      justifyContent={['center', 'flex-start']}
-      px={[0, 4]}
-      image='/static/kelley.jpg'
-    >
-      <Flex flexDirection='column' alignItems={['center', 'flex-start']}>
-        <HeroText
-          fontSize={[6, 8]}
-          py={1}
-          px={3}
-          mb={2}
-          css={{ fontWeight: 900 }}
-        >
-          BeaverHacks
-        </HeroText>
-        <HeroText py={2} px={3} fontSize={[4, 5]} css={{ fontWeight: 100 }}>
-          Make something cool in a weekend
-        </HeroText>
-      </Flex>
-    </Hero>
-    <HalfAndHalf alignItems='flex-start'>
-      <Flex
+const HomePage = ({ start, end, status }) => {
+  let eventHeader
+  switch (status) {
+    case 'in progress':
+      eventHeader = 'Event In Progress'
+      break
+    case 'past':
+      eventHeader = 'Previous Event'
+      break
+    default:
+      eventHeader = 'Next Event'
+  }
+  return (
+    <Box as='main' width={[1]}>
+      <Hero
         alignItems='center'
-        flexDirection='column'
-        width={1}
-        // px={[2, 3]}
-        css={{ textAlign: 'center' }}
+        justifyContent={['center', 'flex-start']}
+        px={[0, 4]}
+        image='/static/kelley.jpg'
       >
-        <Flex flexDirection='column' width={[1, 2 / 3]}>
-          <Heading>Join Us</Heading>
-          <Text>
-            BeaverHacks is a <Highlight>hackathon event</Highlight> for Oregon
-            State University students, organized by the OSU Hackathon Club.
-          </Text>
-        </Flex>
-      </Flex>
-      <Flex width={1} alignItems='center' flexDirection='column'>
-        <Heading>Next Event</Heading>
-        <Highlight mt={0}>June 20th, 2019</Highlight> to{' '}
-        <Highlight>June 23rd, 2019</Highlight>
-        <Flex>
-          <Button
-            mt={3}
-            color='black'
-            bg='transparent'
-            border='3px solid orange'
-            borderColor='black'
+        <Flex flexDirection='column' alignItems={['center', 'flex-start']}>
+          <HeroText
+            fontSize={[6, 8]}
+            py={1}
+            px={3}
+            mb={2}
+            css={{ fontWeight: 900 }}
           >
-            Learn More
-          </Button>
-          <Button
-            mt={3}
-            ml={2}
-            color='white'
-            bg='orange'
-            // border='3px solid orange'
-            // borderColor='black'
-          >
-            Sign Up
-          </Button>
+            BeaverHacks
+          </HeroText>
+          <HeroText py={2} px={3} fontSize={[4, 5]} css={{ fontWeight: 100 }}>
+            Make something cool in a weekend
+          </HeroText>
         </Flex>
-      </Flex>
-    </HalfAndHalf>
-  </Box>
-)
+      </Hero>
+      <HalfAndHalf alignItems='flex-start'>
+        <Flex
+          alignItems='center'
+          flexDirection='column'
+          width={1}
+          // px={[2, 3]}
+          css={{ textAlign: 'center' }}
+        >
+          <Flex flexDirection='column' width={[1, 2 / 3]}>
+            <Heading>Join Us</Heading>
+            <Text>
+              BeaverHacks is a <Highlight>hackathon event</Highlight> for Oregon
+              State University students, organized by the OSU Hackathon Club.
+            </Text>
+          </Flex>
+        </Flex>
+        <Flex width={1} alignItems='center' flexDirection='column'>
+          <Heading>{eventHeader}</Heading>
+          <Highlight>{start}</Highlight>
+          <Text alone> to </Text>
+          <Highlight>{end}</Highlight>
+          <Flex>
+            <Button
+              mt={3}
+              color='black'
+              bg='transparent'
+              border='3px solid orange'
+              borderColor='black'
+            >
+              Learn More
+            </Button>
+            <Button
+              mt={3}
+              ml={2}
+              color='white'
+              bg='orange'
+              // border='3px solid orange'
+              // borderColor='black'
+            >
+              Sign Up
+            </Button>
+          </Flex>
+        </Flex>
+      </HalfAndHalf>
+    </Box>
+  )
+}
+
+HomePage.getInitialProps = async ({ req, res }) => {
+  let status = 'upcoming'
+  const data = await ky(
+    `${
+      req
+        ? `${req.headers['x-forwarded-proto']}://${
+          req.headers['x-forwarded-host']
+        }`
+        : window.location.origin
+    }/api/dates`
+  ).json()
+  console.log(data)
+  const start = new Date(data.start)
+  const end = new Date(data.end)
+  if (start < Date.now() && end > Date.now()) {
+    status = 'in progress'
+  } else if (end < Date.now()) {
+    status = 'past'
+  }
+
+  if (res) {
+    res.setHeader('Cache-Control', 's-maxage=36000, stale-while-revalidate')
+  }
+
+  return {
+    start: start.toDateString(),
+    end: end.toDateString(),
+    status
+  }
+}
+
+HomePage.propTypes = {
+  start: PropTypes.string,
+  end: PropTypes.string,
+  status: PropTypes.oneOf(['upcoming', 'in progress', 'past'])
+}
+
+export default HomePage
