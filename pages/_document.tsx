e3d8a699779370ba7375c5bbc55f4dbb33ee4d01
Graham Barber <grahammbarber@gmail.com>
2019-10-23 17:25:37 -0700

    fix(document): type getInitialProps

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,19 +1,21 @@
-import Document, { Head, Main, NextScript } from "next/document";
+import Document, {
+  Head,
+  Main,
+  NextScript,
+  DocumentContext
+} from 'next/document'
 
 export default class MyDocument extends Document {
-  static async getInitialProps(ctx) {
-    const initialProps = await Document.getInitialProps(ctx);
-    return { ...initialProps };
+  static async getInitialProps (ctx: DocumentContext) {
+    const initialProps = await Document.getInitialProps(ctx)
+    return { ...initialProps }
   }
 
-  render() {
+  render () {
     return (
       <html>
         <Head>
-          <link
-            href="https://fonts.googleapis.com/css?family=Roboto:100,400,700,900"
-            rel="stylesheet"
-          />
+          <link href="https://rsms.me/inter/inter.css" rel="stylesheet" />
           <link
             href="https://fonts.googleapis.com/css?family=Roboto+Slab"
             rel="stylesheet"
@@ -24,6 +26,6 @@ export default class MyDocument extends Document {
           <NextScript />
         </body>
       </html>
-    );
+    )
   }
 }
